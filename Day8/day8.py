52e6815c836604d9c6a41016af7ab6ba2eb093b4
Jens <Jens@DESKTOP-4220AO5.fritz.box>
2021-05-17 19:32:01 +0200

    day8

--- Day8/day8.py
+++ Day8/day8.py
@@ -3,3 +3,41 @@ Created on 17.02.2021
 
 @author: jens
 '''
+
+person = {
+    'first_name':'Jens',
+    'last_name':'Hardt',
+    'age':42,
+    'country':'Mordor',
+    'is_married':False,
+    'skills':['Java', 'C++', 'COBOL', 'MongoDB', 'DB2', 'Oracle', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'12345'
+    }
+}
+
+print(len(person))
+
+print(person.get('country'))
+print(len(person.get('skills')))
+print(person.get('address').get('street'))
+print(person.get('city'))
+# print(person.get('city').get('area')) - AttributeError: 'NoneType' object has no attribute 'get'
+
+person['address']['city'] = 'Mars'
+person['skills'].append('SpringBoot')
+
+print(person)
+
+person.pop('is_married')
+
+print(person)
+
+# person.pop('is_married') - KeyError: 'is_married'
+
+values = person.values()
+print(values)
+
+keys = person.keys()
+print(keys)
